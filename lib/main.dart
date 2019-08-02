4b05e08e5e680fe87a1963d104bc3d536e6cf4b6
Thomas Qvidahl <thomas.qvidahl@telenor.com>
2019-08-02 15:14:01 +0200

    add: cards to make up the ui. add: styling to cards. add: list of cliches to use for the bingo.

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+final TextStyle cardStyle = TextStyle(
+  fontSize: 10.0,
+  fontWeight: FontWeight.w100,
+);
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-        home: BingoPage(),
-
+      home: BingoPage(),
     );
   }
 }
@@ -22,64 +26,110 @@ class BingoPage extends StatefulWidget {
 class _BingoPageState extends State<BingoPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Text('1'),
-              Text('2'),
-              Text('3'),
-              Text('4'),
-              Text('5'),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Text('1'),
-              Text('2'),
-              Text('3'),
-              Text('4'),
-              Text('5'),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Text('1'),
-              Text('2'),
-              Text('3'),
-              Text('4'),
-              Text('5'),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Text('1'),
-              Text('2'),
-              Text('3'),
-              Text('4'),
-              Text('5'),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Text('1'),
-              Text('2'),
-              Text('3'),
-              Text('4'),
-              Text('5'),
-            ],
-          ),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+      title: Text('Conference Call Bingo'),
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: BingoCard(
+                    cliches[0],
+                    onPress: null,
+                  ),
+                ),
+                Expanded(
+                  child: BingoCard(
+                    cliches[1],
+                    onPress: null,
+                  ),
+                ),
+                Expanded(
+                  child: BingoCard(
+                    cliches[2],
+                    onPress: null,
+                  ),
+                ),
+                Expanded(
+                  child: BingoCard(
+                    cliches[3],
+                    onPress: null,
+                  ),
+                ),
+                Expanded(
+                  child: BingoCard(
+                    cliches[4],
+                    onPress: null,
+                  ),
+                ),
+              ],
+            ),
+
+          ],
+        ),
       ),
     );
   }
 }
 
+class BingoCard extends StatelessWidget {
+  BingoCard(this.text, {this.onPress});
 
+
+  final Function onPress;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPress,
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: Container(
+          child: Text(
+            text,
+            style: cardStyle,
+          ),
+          margin: EdgeInsets.all(2.0),
+          decoration: BoxDecoration(
+            color: Colors.grey[900],
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+final List<String> cliches = [
+  'Hi, who just joined?',
+  'Can you email that to everyone?',
+  '___, are you there?',
+  'Uh, ___, you\'re still sharing..',
+  'Hey guys, I have to jump to another call',
+  '(sound of someone typing, possibly with a hammer)',
+  '(loud, painful echo/feedback',
+  '(child or animal noises',
+  'Hi, can you hear me?',
+  '..no, it\'s still loading',
+  'Next slide, please..',
+  'Can everyone go on mute?',
+  'I\'m sorry, I was on mute..',
+  '(for overtalkers)\nSorry, go ahead.',
+  'Hello?\nHello!?',
+  'So, (fades out), I can (unintelligible) by (cuts out) ok?',
+  'Sorry I\'m late <insert lame excuse>',
+  'I have a hard stop at..',
+  'I\'m sorry, you cut out there..',
+  'Can we take this offline?',
+  'I\'ll have to get back to you',
+  'Can everyone see my screen?',
+  'Sorry, I was having connection issues..',
+  'I think there is a lag..',
+  'Sorry I didn\'t catch that, can you repeat?'
+];
